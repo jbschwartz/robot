a50b07e2aada162f5560adbb3df8b0707f5e939d
James Schwartz <james@schwartz.engineer>
2017-12-16 14:04:57 -0500

    Dual operator+ implementation

--- include/dual.h
+++ include/dual.h
@@ -10,6 +10,8 @@ public:
 };
 
 template <typename T>
-Dual<T> operator+(const Dual<T>& a, const Dual<T>& b);
+Dual<T> operator+(const Dual<T>& a, const Dual<T>& b) {
+  return Dual<T>(a.r + b.r, a.d + b.d);
+};
 
 #endif /* __DUAL_H__ */
