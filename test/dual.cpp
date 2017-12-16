4f8d281e5b19563706cd32bf426979fb8f8c901b
James Schwartz <james@schwartz.engineer>
2017-12-16 16:37:00 -0500

    Refactor dual test into number and quaternion sections

--- test/dual.cpp
+++ test/dual.cpp
@@ -2,34 +2,40 @@
 #include "../include/dual.h"
 #include "../include/quaternion.h"
 
-TEST_CASE("Dual Quaternions") {
-  const auto r = Quaternion(1, 2, 3, 4);
-  const auto d = Quaternion(0, 1, 2, 3);
+TEST_CASE("Dual") {
+  SECTION("Number") {
 
-  const auto a = Dual<Quaternion>();
-  const auto b = Dual<Quaternion>(r, d);
-
-  SECTION("add") {
-    const auto c = b + b;
-    REQUIRE(c.r == (r + r));
-    REQUIRE(c.d == (d + d));
   }
 
-  SECTION("multiply") {
-    const auto c = b * b;
-    REQUIRE(c.r == (r * r));
-    REQUIRE(c.d == (r * d + r * d));
-  }
+  SECTION("Quaternions") {
+    const auto r = Quaternion(1, 2, 3, 4);
+    const auto d = Quaternion(0, 1, 2, 3);
 
-  SECTION("scalar multiply") {
-    const Real s = 4;
-    const auto c = s * b;
-    const auto e = b * s;
+    const auto a = Dual<Quaternion>();
+    const auto b = Dual<Quaternion>(r, d);
 
-    REQUIRE(c.r == s * r);
-    REQUIRE(e.r == s * r);
+    SECTION("add") {
+      const auto c = b + b;
+      REQUIRE(c.r == (r + r));
+      REQUIRE(c.d == (d + d));
+    }
 
-    REQUIRE(c.d == s * d);
-    REQUIRE(e.d == s * d);
+    SECTION("multiply") {
+      const auto c = b * b;
+      REQUIRE(c.r == (r * r));
+      REQUIRE(c.d == (r * d + r * d));
+    }
+
+    SECTION("scalar multiply") {
+      const Real s = 4;
+      const auto c = s * b;
+      const auto e = b * s;
+
+      REQUIRE(c.r == s * r);
+      REQUIRE(e.r == s * r);
+
+      REQUIRE(c.d == s * d);
+      REQUIRE(e.d == s * d);
+    }
   }
 }
