9c80666fc1ff2317aa9868ef07dcc188c93aaf9a
James Schwartz <james@schwartz.engineer>
2017-12-17 14:10:01 -0500

    Clean up dual tests. Found error in dual multiplication code :open_mouth:

--- test/dual.cpp
+++ test/dual.cpp
@@ -18,52 +18,64 @@ TEST_CASE("Dual") {
   }
 
   SECTION("Quaternions") {
-    const auto r = Quaternion(1, 2, 3, 4);
-    const auto d = Quaternion(0, 1, 2, 3);
+    const auto r1 = Quaternion(1, 2, 3, 4);
+    const auto d1 = Quaternion(0, 1, 2, 3);
 
-    const auto a = Dual<Quaternion>();
-    const auto b = Dual<Quaternion>(r, d);
+    const auto r2 = Quaternion(0, -1, 0, 0);
+    const auto d2 = Quaternion(1, 3, -4, 5);
+
+    const auto q1 = Dual<Quaternion>(r1, d1);
+    const auto q2 = Dual<Quaternion>(r2, d2);
 
     SECTION("add") {
-      const auto c = b + b;
-      REQUIRE(c.r == (r + r));
-      REQUIRE(c.d == (d + d));
+      const auto result = q1 + q2;
+      const auto expected = Dual<Quaternion>(r1 + r2, d1 + d2);
+
+      REQUIRE(result.r == expected.r);
+      REQUIRE(result.d == expected.d);
     }
 
     SECTION("multiply") {
-      const auto c = b * b;
-      REQUIRE(c.r == (r * r));
-      REQUIRE(c.d == (r * d + r * d));
+      const auto result = q1 * q2;
+      const auto expected = Dual<Quaternion>(r1 * r2, r1 * d2 + d1 * r2);
+
+      REQUIRE(result.r == expected.r);
+      REQUIRE(result.d == expected.d);
     }
 
     SECTION("scalar multiply") {
       const Real s = 4;
-      const auto c = s * b;
-      const auto e = b * s;
 
-      REQUIRE(c.r == s * r);
-      REQUIRE(e.r == s * r);
+      const auto resultPre = s * q1;
+      const auto expected = Dual<Quaternion>(s * r1, s * d1);
 
-      REQUIRE(c.d == s * d);
-      REQUIRE(e.d == s * d);
+      REQUIRE(resultPre.r == expected.r);
+      REQUIRE(resultPre.d == expected.d);
+
+      const auto resultPost = q1 * s;
+      REQUIRE(resultPost.r == expected.r);
+      REQUIRE(resultPost.d == expected.d);
     }
 
     SECTION("equality") {
-      const auto c = Dual<Quaternion>(r, d);
+      const auto result = Dual<Quaternion>(r1, d1);
+      const auto expected = q1;
 
-      REQUIRE(c == b);
+      REQUIRE(result == expected);
     }
 
     SECTION("conjugate") {
-      const auto c = conjugate(b);
+      const auto result = conjugate(q1);
+      const auto expected = Dual<Quaternion>(conjugate(r1), conjugate(d1));
 
-      REQUIRE(c.r == conjugate(r));
-      REQUIRE(c.d == conjugate(d));
+      REQUIRE(result == expected);
     }
 
-    SECTION("norm")  {
-      const auto n = b.norm();
-      REQUIRE(n == b * conjugate(b));
+    SECTION("norm") {
+      const auto result = q1.norm();
+      const auto expected = q1 * conjugate(q1);
+
+      REQUIRE(result == expected);
     }
   }
 }
