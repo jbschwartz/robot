0842b54b847d52fa9a0f0588dca70e14a284c498
James Schwartz <james@schwartz.engineer>
2017-12-17 11:24:28 -0500

    First vector class tests

---
+++ test/vector.cpp
@@ -0,0 +1,13 @@
+#include "third_party/catch.hpp"
+#include "vector.h"
+
+TEST_CASE("Vector") {
+  const auto v1 = Vector3();
+  const auto v2 = Vector3({-1, 2, -3});
+
+  SECTION("defaults to zero vector") {
+    REQUIRE(v1[0] == 0);
+    REQUIRE(v1[1] == 0);
+    REQUIRE(v1[2] == 0);
+  }
+}
