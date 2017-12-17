c1c367e1bb1ee4954d9c8c98c02eac137760695b
James Schwartz <james@schwartz.engineer>
2017-12-17 10:15:00 -0500

    Unit conversion helper function implementation

---
+++ include/utilities.h
@@ -0,0 +1,13 @@
+#ifndef __UTILITIES_H_
+#define __UTILITIES_H_
+
+#include "typedefs.h"
+
+#include <cmath>
+
+const Real PI = 3.141592653589793;
+
+Real toRadians(const Real& degrees);
+Real toDegrees(const Real& radians);
+
+#endif /* __UTILITIES_H_ */
