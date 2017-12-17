9e49dd09c8d48330496bfac8f4a0e5ff3558a2de
James Schwartz <james@schwartz.engineer>
2017-12-17 16:48:01 -0500

    Fix bug in combined transformations :boom:

--- src/transform.cpp
+++ src/transform.cpp
@@ -16,13 +16,15 @@ Transform::Transform(const Vector3& axis, Real angle, const Vector3& translation
   const auto r = Quaternion(c, s * axis);
   const auto t = Quaternion(0, translation / (Real)2);
 
-  this->dual = Dual<Quaternion>(r, t);
+  this->dual = Dual<Quaternion>(r, 0.5 * r * t);
 }
 
 Vector3 Transform::operator()(const Vector3& p) {
-  auto d = Dual<Quaternion>(Quaternion(), Quaternion(0, p));
+  auto d = Dual<Quaternion>(Quaternion(), 0.5 * Quaternion(0, p));
   auto q = this->dual * d * conjugate(this->dual);
-  return Vector3({q.d.x, q.d.y, q.d.z});
+  auto t = 2 * q.d * conjugate(q.r);
+
+  return Vector3({t.x, t.y, t.z});
 }
 
 #ifdef DEBUG
--- test/transform.cpp
+++ test/transform.cpp
@@ -5,6 +5,8 @@ TEST_CASE("Transform") {
   auto pureTranslate = Transform(Vector3({0, 0, 0}), 0, Vector3({4, 2, 6}));
   auto pureRotate = Transform(Vector3({1, 0, 0}), 180, Vector3());
 
+  auto combined = Transform(Vector3({1, 0, 0}), 180, Vector3({4, 2, 6}));
+
   auto point = Vector3({3, 4, 5});
 
   SECTION("translates") {
@@ -34,5 +36,13 @@ TEST_CASE("Transform") {
     REQUIRE(Approx(result[2]) == expected[2]);
   }
 
+  SECTION("combined translate then rotate") {
+    auto result = combined(point);
+    auto expected = Vector3({7, -6, -11});
+
+    REQUIRE(Approx(result[0]) == expected[0]);
+    REQUIRE(Approx(result[1]) == expected[1]);
+    REQUIRE(Approx(result[2]) == expected[2]);
+  }
 
 }
