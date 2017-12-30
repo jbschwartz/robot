a10e80b8f6ba890113cf6117cec5c3c6a2ed15e2
James Schwartz <james@schwartz.engineer>
2017-12-30 13:43:37 -0500

    Add joint limits

--- include/joint.hpp
+++ include/joint.hpp
@@ -3,6 +3,7 @@
 
 #include "typedefs.hpp"
 #include "utilities.hpp"
+#include "vector.hpp"
 
 namespace rbt {
 
@@ -10,8 +11,9 @@ class Transform;
 
 class Joint {
   Real alpha, a, theta, d;
+  Vector2 limits;
 public:
-  Joint(const Real& alpha, const Real& a, const Real& theta, const Real& d) : alpha(alpha), a(a), theta(theta), d(d) {};
+  Joint(const Real& alpha, const Real& a, const Real& theta, const Real& d, const Vector2& limits = Vector2({-360.f, 360.f})) : alpha(alpha), a(a), theta(theta), d(d), limits(limits) {};
   Transform transform(const Real& theta = 0.0);
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,26 @@
 #include "../include/dual.hpp"
 #include "../include/serial.hpp"
 #include "../include/joint.hpp"
+#include "../include/vector.hpp"
 #include "../include/transform.hpp"
 
 #include <iostream>
 
 using rbt::Transform;
 using rbt::Vector3;
+using rbt::Vector2;
 using rbt::Serial;
 using rbt::Joint;
 
 int main() {
 	// ABB IRB120
 	auto s = Serial({
-		Joint(-90,	0, 		0, 		290),
-    Joint(0,   	270,	-90, 	0),
-    Joint(90,		-70, 	180,	0),
-    Joint(-90, 	0,  	0, 		302),
-    Joint(90, 	0, 		0, 		0),
-    Joint(0,   	0, 		0,   	72)
+		Joint(-90,	0, 		0, 		290, 	Vector2({-165, 165})),
+    Joint(0,   	270,	-90, 	0, 		Vector2({-110, 110})),
+    Joint(90,		-70, 	180,	0, 		Vector2({-90, 70})),
+    Joint(-90, 	0,  	0, 		302, 	Vector2({-160, 160})),
+    Joint(90, 	0, 		0, 		0, 		Vector2({-120,120})),
+    Joint(0,   	0, 		0,   	72, 	Vector2({-400,400}))
 	});
 
 	auto result = s.pose({45, 45, 45, 45, 45, 45});
