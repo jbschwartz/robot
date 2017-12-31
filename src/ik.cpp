15b90cf719cb771455accda0505c5ca00dd16012
James Schwartz <james@schwartz.engineer>
2017-12-31 12:55:39 -0500

    Optimize removeIf... function

--- src/ik.cpp
+++ src/ik.cpp
@@ -7,6 +7,9 @@ void removeIfOutsideLimits(Angles& angles, const Vector2& limits) {
   const Real low = limits[0];
   const Real high = limits[1];
 
+  // Optimization: don't bother looping through items if there are infnite limits
+  if(low == -INF && high == INF) return;
+
   const auto last = std::remove_if(angles.begin(), angles.end(), [low, high](const Real& theta) {
     // Do not remove singular values as they represent all values _inside_ range
     if(theta == SINGULAR) return false;
