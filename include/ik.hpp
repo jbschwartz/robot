f40662b744a9732806e5bbc8499638879994f06e
James Schwartz <james@schwartz.engineer>
2017-12-31 14:26:15 -0500

    Refactor waistAngle limits parameter default value into constant

--- include/ik.hpp
+++ include/ik.hpp
@@ -15,10 +15,12 @@ typedef std::vector<Real> Angles;
 static_assert(std::numeric_limits<Real>::has_infinity, "Type Real does not have infinity value");
 const auto SINGULAR = std::numeric_limits<Real>::infinity();
 
+const auto NO_LIMITS = Vector2({-INF, INF});
+
 // Calculate the waist (joint 0) angles satisfying actuator limits for location (x,y)
-Angles waistAngles(const Real&, const Real& y, const Vector2& limits = Vector2({-INF, INF}));
+Angles waistAngles(const Real&, const Real& y, const Vector2& limits = NO_LIMITS);
 // Calculate the waist (joint 0) angles satisfying actuator limits for location (x,y) with a shoulder offset
-Angles waistAngles(const Real&, const Real& y, const Real& offset, const Vector2& limits = Vector2({-INF, INF}));
+Angles waistAngles(const Real&, const Real& y, const Real& offset, const Vector2& limits = NO_LIMITS);
 
 // Remove angles from a vector should the exceed actuator limits
 // TODO: This could potentially be more generic
