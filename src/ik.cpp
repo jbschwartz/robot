c3ff882ca3c4708483630729271372f159777115
James Schwartz <james@schwartz.engineer>
2017-12-31 17:36:20 -0500

    Allow caller to call waistAngles(x, y, offset) with offset = 0. Caller now does not need to check the value of offset in order to decide which function overload to call

--- src/ik.cpp
+++ src/ik.cpp
@@ -29,6 +29,8 @@ AngleSets waistAngles(const Real& x, const Real& y) {
 
 // Project the wrist center onto the XY plane, solve for the angle in the plane with a shoulder offset.
 AngleSets waistAngles(const Real& x, const Real& y, const Real& offset) {
+  if(offset == 0) return waistAngles(x, y);
+
   // Shoulder offsets create potential for unreachable locations (so we check)
   // A point is unreachable is x^2 + y^2 < d^2,
   //   i.e. if the point is "inside" the (circle produced by the) offset shoulder
