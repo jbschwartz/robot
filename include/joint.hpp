986ccaba48a60600a65975e8894513d19ee31bdb
James Schwartz <james@schwartz.engineer>
2018-01-07 13:44:10 -0500

    Implement changes to Joint class necessary for the positionSets function

--- include/joint.hpp
+++ include/joint.hpp
@@ -11,10 +11,14 @@ class Transform;
 
 class Joint {
   Real alpha, a, theta, d;
-  Vector2 limits;
+  Vector2 lims;
 public:
-  Joint(const Real& alpha, const Real& a, const Real& theta, const Real& d, const Vector2& limits = Vector2({-360.f, 360.f})) : alpha(alpha), a(a), theta(theta), d(d), limits(limits) {};
+  Joint(const Real& alpha, const Real& a, const Real& theta, const Real& d, const Vector2& limits = Vector2({-360.f, 360.f})) : alpha(alpha), a(a), theta(theta), d(d), lims(limits) {};
   Transform transform(const Real& theta = 0.0);
+
+  Real offset() const { return this->d; };
+  Real length() const { return this->a; };
+  Vector2 limits() const { return this->lims; };
 };
 
 }
