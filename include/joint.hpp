ace954794da0bd2b790f938c483cfdf903ff9fb4
James Schwartz <james@schwartz.engineer>
2018-01-28 11:40:55 -0500

    Store joint limits in radians

--- include/joint.hpp
+++ include/joint.hpp
@@ -13,7 +13,10 @@ class Joint {
   Real alpha, a, theta, d;
   Vector2 lims;
 public:
-  Joint(const Real& alpha, const Real& a, const Real& theta, const Real& d, const Vector2& limits = Vector2({-360.f, 360.f})) : alpha(alpha), a(a), theta(theta), d(d), lims(limits) {};
+  Joint(const Real& alpha, const Real& a, const Real& theta, const Real& d, const Vector2& limits = Vector2({-360.f, 360.f})) : alpha(alpha), a(a), theta(theta), d(d), lims(limits) {
+    lims[0] = toRadians(lims[0]);
+    lims[1] = toRadians(lims[1]);
+  };
   Transform transform(const Real& theta = 0.0) const;
 
   Real offset() const { return this->d; };
