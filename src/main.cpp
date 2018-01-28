5e631d307d39f5ff002b06a552d7b97c010a0b5c
James Schwartz <james@schwartz.engineer>
2018-01-28 12:10:06 -0500

    Fix DH parameters and remove tabs for spaces on main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,18 +14,18 @@ using rbt::Serial;
 using rbt::Joint;
 
 int main() {
-	// ABB IRB120
-	auto s = Serial({
-		Joint(-90,	0, 		0, 		290, 	Vector2({-165, 165})),
-    Joint(0,   	270,	-90, 	0, 		Vector2({-110, 110})),
-    Joint(90,		-70, 	180,	0, 		Vector2({-90, 70})),
-    Joint(-90, 	0,  	0, 		302, 	Vector2({-160, 160})),
-    Joint(90, 	0, 		0, 		0, 		Vector2({-120,120})),
-    Joint(0,   	0, 		0,   	72, 	Vector2({-400,400}))
-	});
+  // ABB IRB120
+  auto s = Serial({
+    Joint( -90,    0,    0,  290, Vector2({ -165, 165 })),
+    Joint(   0,  270,  -90,    0, Vector2({ -110, 110 })),
+    Joint( -90,   70,    0,    0, Vector2({  -90, 70  })),
+    Joint(  90,    0,    0,  302, Vector2({ -160, 160 })),
+    Joint( -90,    0,    0,    0, Vector2({ -120, 120 })),
+    Joint(   0,    0,  180,   72, Vector2({ -400, 400 }))
+  });
 
-	auto result = s.pose({45, 45, 45, 45, 45, 45});
+  auto result = s.pose({45, 45, 45, 45, 45, 45});
 
-	std::cout << result.position() << std::endl;
-	std::cout << result.orientation() << std::endl;
+  std::cout << result.position() << std::endl;
+  std::cout << result.orientation() << std::endl;
 }
