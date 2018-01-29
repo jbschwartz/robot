7d5bfd58378b130088c62f63244b0f12803aa8f6
James Schwartz <james@schwartz.engineer>
2018-01-28 20:43:00 -0500

    Hacky Catch2 Frame matcher

---
+++ test/matchers/frame.hpp
@@ -0,0 +1,39 @@
+#include "../third_party/catch.hpp"
+#include "../include/frame.hpp"
+#include "../include/quaternion.hpp"
+
+using rbt::Frame;
+using rbt::Quaternion;
+
+class FrameMatcher : public Catch::MatcherBase<Frame>
+{
+  Quaternion position;
+  Quaternion orientation;
+
+public:
+  FrameMatcher(const Frame& a) : position(a.pose().d), orientation(a.pose().r) {};
+
+  virtual bool match(const Frame& b) const override {
+    const auto bPosition = b.pose().d;
+    const auto bOrientation = b.pose().r;
+
+    // I can probably do better than this... right...?
+    return bPosition.r == Approx(position.r) &&
+      bPosition.x == Approx(position.x) &&
+      bPosition.y == Approx(position.y) &&
+      bPosition.z == Approx(position.z) &&
+      bOrientation.r == Approx(orientation.r) &&
+      bOrientation.x == Approx(orientation.x) &&
+      bOrientation.y == Approx(orientation.y) &&
+      bOrientation.z == Approx(orientation.z);
+  }
+
+  virtual std::string describe() const override {
+      std::ostringstream ss;
+      return ss.str();
+  }
+};
+
+inline FrameMatcher FrameEquals(const Frame& a) {
+  return FrameMatcher(a);
+}
