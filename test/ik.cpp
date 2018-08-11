b0e36d9e565e51b4d28177c4c35321453d31ce0c
James Schwartz <james@schwartz.engineer>
2018-08-11 13:16:31 -0400

    Fixes error in 85be5ef

--- test/ik.cpp
+++ test/ik.cpp
@@ -188,7 +188,7 @@ TEST_CASE("Inverse Kinematics") {
     const auto expected = Angles({ toRadians(45), toRadians(45), toRadians(45), toRadians(45), toRadians(45), toRadians(45) });
 
     const auto frame = ABB_IRB_120.pose(expected);
-    const auto result = angles(frame, ABB_IRB_120);
+    const auto result = angles(frame, ABB_IRB_120.joints());
 
     CHECK_THAT(result.front(), ComponentsEqual(expected));
   }
