88e8a180c7de6a238868b495ee2eaf2fd52a2133
James Schwartz <jbschwartz@users.noreply.github.com>
2018-08-14 20:18:11 -0400

    Add test for theta DH waist parameter in solveWaist

--- test/ik/waist.cpp
+++ test/ik/waist.cpp
@@ -11,29 +11,33 @@ TEST_CASE("solveWaist") {
   const std::vector<Real> angles = { 0, 30, 90, 150, -120, -90, -30 };
   const Real armLength = 3;
 
-  SECTION("calculates waist angles (-PI, PI] with and without shoulder offset") {
+  SECTION("calculates waist angles (-PI, PI] with and without shoulder offset, with and without thetas") {
     // Solve with and without a shoulder offset:
     //    Prescribe the alpha angle and solve for shoulderOffset
     //    This makes the expected results easy to reason about
     const std::vector<Real> alphas = { 0, 15, -15 };
+    const std::vector<Real> thetas = { 0, 15, -15 };
 
-    for(auto alpha : alphas) {
-      alpha = toRadians(alpha);
-      const Real shoulderOffset = armLength * std::sin(alpha);
+    for(auto theta : thetas) {
+      theta = toRadians(theta);
+      for(auto alpha : alphas) {
+        alpha = toRadians(alpha);
+        const Real shoulderOffset = armLength * std::sin(alpha);
 
-      for(auto angle : angles) {
-        angle = toRadians(angle);
-        // Calculate target position
-        const Real y = armLength * sin(angle);
-        const Real x = armLength * cos(angle);
+        for(auto angle : angles) {
+          angle = toRadians(angle);
+          // Calculate target position
+          const Real y = armLength * sin(angle);
+          const Real x = armLength * cos(angle);
 
-        const auto result = solveWaist(x, y, 0, shoulderOffset);
-        const std::vector<Real> expected = {
-          minusPiToPi(angle - alpha),
-          minusPiToPi(angle + alpha + PI)
-        };
+          const auto result = solveWaist(x, y, theta, shoulderOffset);
+          const std::vector<Real> expected = {
+            minusPiToPi(angle - alpha - theta),
+            minusPiToPi(angle + alpha - theta + PI)
+          };
 
-        CHECK_THAT(result, ComponentsEqual(expected));
+          CHECK_THAT(result, ComponentsEqual(expected));
+        }
       }
     }
   }
