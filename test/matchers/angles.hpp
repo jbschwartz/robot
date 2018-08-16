130558c55f88c1de224f1e7388f5bc0df8152b81
James Schwartz <jbschwartz@users.noreply.github.com>
2018-08-15 21:10:53 -0400

    Squash bug in Angles matcher that caused a positive match when the expected value vector (a) was empty. Switch to approxEqual for floating point comparison

--- test/matchers/angles.hpp
+++ test/matchers/angles.hpp
@@ -1,7 +1,9 @@
 #include "../third_party/catch.hpp"
 #include "../include/ik.hpp"
+#include "../include/utilities.hpp"
 
 using rbt::ik::Angles;
+using rbt::approxEqual;
 
 class AnglesMatcher : public Catch::MatcherBase<Angles>
 {
@@ -10,17 +12,24 @@ public:
   AnglesMatcher(const Angles& a) : a(a) {};
 
   virtual bool match(const Angles& b) const override {
+    if(b.size() != a.size()) return false;
+
     auto bElement = b.begin();
 
     for(const auto& aElement : this->a) {
-      if(Approx(aElement) != *bElement++) return false;
+      if(!approxEqual(aElement, *bElement++)) return false;
     }
+
     return true;
   }
 
   virtual std::string describe() const override {
       std::ostringstream ss;
-      ss << "is not equal to ";
+      ss << "is not equal to { ";
+      for(auto angle : a) {
+        ss << angle << " ";
+      }
+      ss << "}";
       return ss.str();
   }
 };
