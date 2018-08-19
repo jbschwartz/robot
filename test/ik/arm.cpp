18b72be32a69a85e66f2269a54066307987ef2b9
James Schwartz <jbschwartz@users.noreply.github.com>
2018-08-19 10:59:05 -0400

    Fix failing test

--- test/ik/arm.cpp
+++ test/ik/arm.cpp
@@ -1,6 +1,8 @@
 #include "../third_party/catch.hpp"
 #include "../matchers/angles.hpp"
+#include "../robots/abb_irb_120.hpp"
 #include "../../include/ik.hpp"
+#include "../../include/serial.hpp"
 #include "../../include/utilities.hpp"
 #include "../../include/typedefs.hpp"
 
@@ -8,8 +10,15 @@ using namespace rbt;
 using namespace rbt::ik;
 
 TEST_CASE("solveArm") {
-  const Vector3 target = Vector3({ 302, 0, 630 });
-  const auto result = solveArm(target, ABB_IRB_120.joints());
+  // Corresponds to all joint angles set to 45 degrees
+  const Vector3 target = Vector3({ 184.497, 184.497, 178.919 });
+
+  const auto upperArmLength = ABB_IRB_120.upperArmLength();
+  const auto foreArmLength = ABB_IRB_120.foreArmLength();
+  const auto shoulderWristOffset = ABB_IRB_120.shoulderWristOffset();
+  const auto shoulderZ = ABB_IRB_120.shoulderZ();
+
+  const auto result = solveArm(target, upperArmLength, foreArmLength, shoulderWristOffset, shoulderZ);
 
   SECTION("calculates position solutions to inverse kinematics") {
     const auto expectedSize = 4;
