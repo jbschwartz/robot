624d9473c43846cf42606512452478fbb871a240
James Schwartz <jbschwartz@users.noreply.github.com>
2019-02-03 12:39:55 -0500

    Implement solveArm test.

--- test/ik/arm.cpp
+++ test/ik/arm.cpp
@@ -9,23 +9,32 @@
 using namespace rbt;
 using namespace rbt::ik;
 
+// TODO: Need a way to test solveArm specifically without having to make calls to other functionality (transformAngles..., removeIfBeyond...)
 TEST_CASE("solveArm") {
   // Corresponds to all joint angles set to 45 degrees
-  const Vector3 target = Vector3({ 184.497, 184.497, 178.919 });
+  const auto expected = Angles({ toRadians(45), toRadians(45), toRadians(45), toRadians(45), toRadians(45), toRadians(45) });
+  const auto expectedSize = 1;
+
+  const auto pose = ABB_IRB_120.pose(expected);
+  const Vector3 target = wristCenterPoint(pose, ABB_IRB_120.wristLength());
 
   const auto upperArmLength = ABB_IRB_120.upperArmLength();
   const auto foreArmLength = ABB_IRB_120.foreArmLength();
   const auto shoulderWristOffset = ABB_IRB_120.shoulderWristOffset();
   const auto shoulderZ = ABB_IRB_120.shoulderZ();
 
-  const auto result = solveArm(target, upperArmLength, foreArmLength, shoulderWristOffset, shoulderZ);
+  auto result = solveArm(target, upperArmLength, foreArmLength, shoulderWristOffset, shoulderZ);
 
-  SECTION("calculates position solutions to inverse kinematics") {
-    const auto expectedSize = 4;
+  transformAnglesToRobot(result, ABB_IRB_120);
+
+  removeIfBeyondLimits(result, ABB_IRB_120.limits());
+
+  SECTION("calculates joint angles for wristCenter position") {
     REQUIRE(result.size() == expectedSize);
 
-    SECTION("needs tests") {
-      REQUIRE(true == true);
-    }
+    std::for_each(result.front().begin(), result.front().end(), [expected](auto angle) {
+      REQUIRE(angle == Approx(expected.front()));
+    });
+
   }
 }
