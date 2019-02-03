ee40e9e16d195ba7ff9f4bba5e5b1c2210611a8f
James Schwartz <jbschwartz@users.noreply.github.com>
2019-02-03 12:54:33 -0500

    Catch main up to changes made in the library.
    Mostly functions now take radians instead of degrees.

--- src/main.cpp
+++ src/main.cpp
@@ -12,19 +12,21 @@ using rbt::Vector3;
 using rbt::Vector2;
 using rbt::Serial;
 using rbt::Joint;
+using rbt::toRadians;
 
 int main() {
   // ABB IRB120
-  auto s = Serial({
-    Joint( -90,    0,    0,  290, Vector2({ -165, 165 })),
-    Joint(   0,  270,  -90,    0, Vector2({ -110, 110 })),
-    Joint( -90,   70,    0,    0, Vector2({  -90, 70  })),
-    Joint(  90,    0,    0,  302, Vector2({ -160, 160 })),
-    Joint( -90,    0,    0,    0, Vector2({ -120, 120 })),
-    Joint(   0,    0,  180,   72, Vector2({ -400, 400 }))
+  const auto ABB_IRB_120 = Serial({
+    Joint(toRadians( -90),    0, toRadians(   0),  290, Vector2({ toRadians(-165), toRadians(165) })),
+    Joint(toRadians(   0),  270, toRadians( -90),    0, Vector2({ toRadians(-110), toRadians(110) })),
+    Joint(toRadians( -90),   70, toRadians(   0),    0, Vector2({ toRadians(-110), toRadians(70)  })),
+    Joint(toRadians(  90),    0, toRadians(   0),  302, Vector2({ toRadians(-160), toRadians(160) })),
+    Joint(toRadians( -90),    0, toRadians(   0),    0, Vector2({ toRadians(-120), toRadians(120) })),
+    Joint(toRadians(   0),    0, toRadians( 180),   72, Vector2({ toRadians(-400), toRadians(400) }))
   });
 
-  auto result = s.pose({45, 45, 45, 45, 45, 45});
+  const auto angle = toRadians(45);
+  auto result = ABB_IRB_120.pose({angle, angle, angle, angle, angle, angle});
 
   std::cout << result.position() << std::endl;
   std::cout << result.orientation() << std::endl;
